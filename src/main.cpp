6330fd16663e38835f6d8dbb9e26cac563fbd6bf
ananshi <ananshi.gla_cs.aiml24@gla.ac.in>
2026-02-17 08:53:16 +0530

    Add DHT sensor support and configure serial monitor settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 #include <Arduino.h>
+#include <DHT.h>
+#define DHTTYPE DHT11
+#define DHTPIN 2
+DHT dht(DHTPIN, DHTTYPE);
 void setup() {
+    Serial.begin(9600);
+    dht.begin();
 // write your initialization code here
 }
 
 void loop() {
+    float humidity=dht.readHumidity();
+    float temperature=dht.readTemperature();
+    if (isnan(humidity) || isnan(temperature)) {
+        Serial.println("Failed to read from DHT sensor!");
+        delay(2000);
+        return;
+    }
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print(" %|");
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println(" *C");
+    delay(2000);
+
 // write your code here
 }
